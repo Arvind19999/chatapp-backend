9fd77638e0119ff2c3456efa5936e11eaf32be75
Arvind19999 <pas074bct007@wrc.edu.np>
2025-05-04 17:31:09 +0545

    fest(models) : add models for user, message,chat,request

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("backend is testing")
+import express from 'express';
+
+import userRoute from './routes/user.js';
+
+const app = express();
+
+app.use("/user", userRoute);
+
+app.use("/", (req, res) => {
+    res.send("This is Home Page");
+}
+)
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+})
---
+++ controllers/user.js
@@ -0,0 +1,6 @@
+const login = (req,res) =>{
+    res.send("This is the testing of login function")
+}
+
+
+export {login}
